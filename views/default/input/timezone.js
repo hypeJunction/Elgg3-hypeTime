7ef85d04a5dd5daa0ae3c384debd2b5f22ba7eeb
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2026-05-09 14:15:58 +0200

    feat(js): convert AMD modules to ES modules

--- views/default/input/timezone.js
+++ views/default/input/timezone.js
@@ -1,41 +1,39 @@
-define(function (require) {
+import $ from 'jquery';
+import Ajax from 'elgg/Ajax';
 
-	var Ajax = require('elgg/Ajax');
-	var ajax = new Ajax();
+const ajax = new Ajax();
 
-	var cache = [];
+const cache = [];
 
-	function setOptions(self, options) {
-		var options = options || [];
-		var $parent = self.parents('.elgg-input-timezone').eq(0);
-		var $tzIdPicker = $parent.find('select[data-timezone-id]').eq(0);
+function setOptions(self, options) {
+	const options_ = options || [];
+	const $parent = self.parents('.elgg-input-timezone').eq(0);
+	const $tzIdPicker = $parent.find('select[data-timezone-id]').eq(0);
 
-		$tzIdPicker.children('option').not(':selected').remove();
-		$.each(options, function (index, tz) {
-			if ($tzIdPicker.find('[value="' + tz.id + '"]').length === 0) {
-				var $option = $('<option>').attr({value: tz.id}).text(tz.label);
-				$option.appendTo($tzIdPicker);
-			}
-		});
-	}
-
-	$(document).on('change', '.elgg-input-timezone select[data-timezone-country]', function () {
-		var self = $(this);
-		var country = self.val();
-
-		if (cache[country]) {
-			setOptions(self, cache[country]);
-		} else {
-			ajax.path('data/timezones', {
-				data: {
-					country: country
-				},
-				cache: true
-			}).done(function (data) {
-				cache[country] = data;
-				setOptions(self, data);
-			});
+	$tzIdPicker.children('option').not(':selected').remove();
+	$.each(options_, function (index, tz) {
+		if ($tzIdPicker.find('[value="' + tz.id + '"]').length === 0) {
+			const $option = $('<option>').attr({value: tz.id}).text(tz.label);
+			$option.appendTo($tzIdPicker);
 		}
 	});
+}
 
+$(document).on('change', '.elgg-input-timezone select[data-timezone-country]', function () {
+	const self = $(this);
+	const country = self.val();
+
+	if (cache[country]) {
+		setOptions(self, cache[country]);
+	} else {
+		ajax.path('data/timezones', {
+			data: {
+				country: country
+			},
+			cache: true
+		}).done(function (data) {
+			cache[country] = data;
+			setOptions(self, data);
+		});
+	}
 });
